620695ca08178e534b4e0f9edbc94e023256a1fd
Ruben Perez <rubenperez038@gmail.com>
2026-02-26 13:20:58 +0100

    lwt_unattend

--- include/boost/core/detail/lwt_unattended.hpp
+++ include/boost/core/detail/lwt_unattended.hpp
@@ -1,3 +1,8 @@
+// Make the header safe to include from libraries supporting modules
+#if defined(BOOST_IN_MODULE_PURVIEW) && !defined(BOOST_CORE_DETAIL_LWT_UNATTENDED_HPP_INCLUDED)
+#  error "Please #include <boost/core/detail/lwt_unattend.hpp> in your module global fragment"
+#endif
+
 #ifndef BOOST_CORE_DETAIL_LWT_UNATTENDED_HPP_INCLUDED
 #define BOOST_CORE_DETAIL_LWT_UNATTENDED_HPP_INCLUDED
 
@@ -5,9 +10,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifndef BOOST_USE_MODULES
 #include <cstdlib>
-#include <boost/core/detail/crtdbg.hpp>
+#if defined(_MSC_VER) && defined(_CPPLIB_VER) && defined(_DEBUG)
+# include <crtdbg.h>
 #endif
 
 namespace boost
